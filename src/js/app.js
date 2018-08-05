691bc49a5741a66d1aa80d01736bb5a9cf264c53
Cote <majovardon@gmail.com>
2018-08-04 20:47:30 -0400

    Fetch y obtencion de data de la tarjeta

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+//Haciendo el fetch a la url
+fetch('http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=75641459')
+  .then(response => response.json())
+  .then(bipJSON => {
+    bip(bipJSON);
+    bipCalculator(bipJSON);
+  })
+  .catch(error => {
+    console.error('Problemas para obtener la información');
+    console.error('Tipo de error: ' + error.stack);
+  });
+
+//Sacando la información relevante del JSON e imprimiendola en la página
+const bip = (bipJSON) => {
+  askedBalance.innerHTML = '';
+  for (let i in bipJSON) {
+    askedBalance.innerHTML = `
+  ${bipJSON['saldoTarjeta']}`
+  }
+}
+
+//Seleccionando la tarifa
+selectedRate = () => {
+  const selector = document.getElementById('rate');
+  let value = selector[selector.selectedIndex].value;
+  document.getElementById('askedCost').innerHTML = '$' + value;
+}
+
+//Calculando el balance final(no funcional)
+const bipCalculator = (bipJSON) => {
+  const cardBalance = bipJSON['saldoTarjeta'];
+    /*
+    let interger = parseInt(cardBalance, 10)
+    console.log(interger)
+  }*/
+  const passajeCost = askedCost.value;
+  const result = parseInt(cardBalance - passajeCost);
+  document.getElementById('finalBalance').innerHTML = '$' + result
+}
+
