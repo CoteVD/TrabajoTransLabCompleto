f9333b6ca2f2c86f68258962efd6cc5461f81b79
Cote <majovardon@gmail.com>
2018-08-06 12:45:13 -0400

    Tests listos, DOM listo y funciones listas; falta agregar . en el número del saldo final

--- src/js/transLabValidate.js
+++ src/js/transLabValidate.js
@@ -1,26 +1,44 @@
-window.validateEmail = function(email){
-  if(email.indexOf('@') < 0){
-      return false;
-  }
+//Validando Email
+window.validateEmail = function (email) {
+    if (email.indexOf('@') < 0) {
+        return false;
+    }
 
-  const tokens = email.split('@');
-  if(tokens.length != 2){ 
-      return false;
-  }
+    const tokens = email.split('@');
+    if (tokens.length != 2) {
+        return false;
+    }
 
-  if(tokens[1].indexOf('.') < 0){
-      return false;
-  }
+    if (tokens[1].indexOf('.') < 0) {
+        return false;
+    }
 
-  const domTokens = tokens[1].split('.');
-  if(domTokens.length != 2){
-      return false;
-  }
-  if(domTokens[0].length < 1){
-      return false
-  }
-  if(domTokens[1].length < 1){
-      return false
-  }
-  return true;
+    const domTokens = tokens[1].split('.');
+    if (domTokens.length != 2) {
+        return false;
+    }
+    if (domTokens[0].length < 1) {
+        return false
+    }
+    if (domTokens[1].length < 1) {
+        return false
+    }
+    return true;
 };
+
+//Validando Password
+window.validatePassword = (password) => {
+    if (password.toString().length <= 8 && password.toString().length >= 6) {
+        if (isNaN(password) == false) {
+            if (!password.toString().trim() == " ") {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
--- test/translab.spec.js
+++ test/translab.spec.js
@@ -1,28 +1,44 @@
 const assert = require('chai').assert;
 global.window = global;
 require('../src/js/transLabValidate');
-describe('Validar emails', ()=>{ 
-  describe('Debería verificar si existe el arroba', ()=>{
-      it('Debería el correo tener solo un arroba', ()=>{
-          assert.equal(validateEmail("asdf@asdf.com"), true); 
-          assert.equal(validateEmail("asdf@@asdf.com"), false);
-          assert.equal(validateEmail("asdf.com"), false);
-      });
-      it('Debería el correo tener un arroba antes del dominio', ()=>{
-          assert.equal(validateEmail(".com@asdf"), false);
-          assert.equal(validateEmail("fabian@laboratoria.la"), true);
-      });
-  });(
-  describe('Debería verificar si tiene un dominio', ()=>{
-      it('Tiene un punto y luego solo caracteres del alfabeto', ()=>{
-          assert.equal(validateEmail('asdf@asdf'), false);
-          assert.equal(validateEmail('asdf@asdf.'), false);
-      });
-  }));
-  describe('Debería verificar que no tenga carácteres raros', ()=>{
+//Testing para validar el email
+describe('Validar emails', () => {
+    describe('Debería verificar si existe el arroba', () => {
+        it('Debería el correo tener solo un arroba', () => {
+            assert.equal(validateEmail("asdf@asdf.com"), true);
+            assert.equal(validateEmail("asdf@@asdf.com"), false);
+            assert.equal(validateEmail("asdf.com"), false);
+        });
+        it('Debería el correo tener un arroba antes del dominio', () => {
+            assert.equal(validateEmail(".com@asdf"), false);
+            assert.equal(validateEmail("fabian@laboratoria.la"), true);
+        });
+    }); (
+        describe('Debería verificar si tiene un dominio', () => {
+            it('Tiene un punto y luego solo caracteres del alfabeto', () => {
+                assert.equal(validateEmail('asdf@asdf'), false);
+                assert.equal(validateEmail('asdf@asdf.'), false);
+            });
+        }));
+});
 
-  });(
-  describe('Debería verificar que tenga al menos 1 caracter antes del arroba', ()=>{
+//Testing para validar la password
+describe('Validar clave', () => {
+    describe('verificar que contenga entre 6 y 8 caracteres, que sean solo numeros y que no tenga espacios en blanco', () => {
+        it('verificar que contenga un maximo de 8 caracteres y minimo 6', () => {
+            assert.equal(validatePassword('12345678'), true);
+            assert.equal(validatePassword('123456'), true);
+            assert.equal(validatePassword('123456789'), false);
+            assert.equal(validatePassword('12345'), false);
+        });
+        it('verificar que contenga solo numeros', () => {
+            assert.equal(validatePassword('12345678'), true);
+            assert.equal(validatePassword('12a4g6'), false);
 
-  }));
+        });
+        it('verificar que no contenga espacios en blanco', () => {
+            assert.equal(validatePassword('12345678'), true);
+            assert.equal(validatePassword('12 34'), false);
+        });
+    });
 });
